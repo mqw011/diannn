62ebd4072d95023a76874c09a5654c559e8a20a6
ssslap <maqsatjumatai@gmail.com>
2026-05-05 00:51:26 +0500

    Configure static export to fix Vercel build trace issue

--- kazakhstan-quiz/next.config.js
+++ kazakhstan-quiz/next.config.js
@@ -1,3 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  // Optional: Change links `/about` to `/about/` if needed, but not strictly necessary here.
+  // trailingSlash: true,
+};
+
 module.exports = nextConfig;
